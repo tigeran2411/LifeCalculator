f5c035112d633068963a727fb020d4c0bbb5c360
esthera <32184416+tigeran2411@users.noreply.github.com>
2018-02-17 23:18:07 -0800

    The Meaning Of Life

---
+++ LifeCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type your birthday. (EX 00/00/00): ");
+            string UserBirthday = Console.ReadLine();
+            DateTime MyBirthday = DateTime.Parse(UserBirthday);
+            TimeSpan myAge = DateTime.Now.Subtract(MyBirthday);
+            Console.WriteLine("You have been alive for " + (int)myAge.TotalDays + " days!");
+            Console.ReadLine();
+        }
+    }
+}
